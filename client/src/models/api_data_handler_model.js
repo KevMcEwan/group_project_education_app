f08fd9593f8f68e3587966020e42275fd361ee29
Christina Davies <christinahdavies@gmail.com>
2018-12-12 15:55:02 +0000

    added objects for country capitals api

--- client/src/models/api_data_handler_model.js
+++ client/src/models/api_data_handler_model.js
@@ -8,12 +8,27 @@ const periodicTableFormatting = (data) => {
   });
 };
 
+const countryCapitalsFormatting = (data) => {
+  return data.map((card) => {
+    return {
+      answer: card.capital,
+      clue: card.name,
+      gameLevel: 1
+    };
+  });
+};
+
+
 
 const ApiDataHandler = {
-   periodicTable: {
+  periodicTable: {
     url: 'https://raw.githubusercontent.com/andrejewski/periodic-table/master/data.json',
     mapAPIDataToCards: periodicTableFormatting
-  }
+  },
+  countryCapitals: {
+    url: 'https://restcountries.eu/rest/v2/all',
+    mapAPIDataToCards: countryCapitalsFormatting
+  };
 };
 
 
